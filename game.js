b6c99e0d780eb59c3545fcad6d2fe90100a0821a
Babydoll-techgirl <ritajack@gmail.com>
2021-06-22 19:22:02 -0400

    Updated game.js

--- game.js
+++ game.js
@@ -20,7 +20,7 @@ fetch ("https://quizapi.io/api/v1/questions?limit=10&apiKey=q6NHZAHRcFHFl0runQZz
 .then(response => response.json()
 .then(result => {
     // let availableQuesions = [...result[0].question];
-    const questionArray = Array.from [result];
+    // const questionArray = Array.from [result];
     const answer1 = document.getElementById("choice1");
     const answer2 = document.getElementById("choice2");
     const answer3 = document.getElementById("choice3");
@@ -35,17 +35,17 @@ fetch ("https://quizapi.io/api/v1/questions?limit=10&apiKey=q6NHZAHRcFHFl0runQZz
     answer5.innerText = result[4].answers.answer_e;
     answer6.innerText = result[4].answers.answer_f;
 
-   if (JSON.stringify(answer5) == " ") {
-       document.getElementById("fifthChoice").hidden;
-   } 
-    console.log(result[4].question);
-    console.log(result[4].answers.answer_a);
-    localStorage.setItem('question', JSON.stringify(result[0].question));
-    console.log(result);
-    nextQuestion () => {
-        questionArray.forEach(index ++);
-    }
-    console.log(nextQuestion());
+//    if (JSON.stringify(answer5) == " ") {
+//        document.getElementById("fifthChoice").hidden;
+//    } 
+//     console.log(result[4].question);
+//     console.log(result[4].answers.answer_a);
+//     localStorage.setItem('question', JSON.stringify(result[0].question));
+//     console.log(result);
+//     nextQuestion () => {
+//         questionArray.forEach(index ++);
+//     }
+    // console.log(nextQuestion());
 }))
 .catch(error => {
     console.log('error')
