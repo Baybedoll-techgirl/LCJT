2bc3341c3765f00c679cba76f99be24528a93308
Laura Medina <lcmedina390@gmail.com>
2021-06-26 12:36:16 -0500

    WORKING FEEDBACK

--- end.js
+++ end.js
@@ -3,7 +3,6 @@ const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 finalScore.innerText = `${mostRecentScore}/50`;
-
 // username.addEventListener('keyup', () => {
 //     saveScoreBtn.disabled = !username.value;
 // });
--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const scoreText = document.getElementById('score');
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 10;
 
-let score = 0;
 let questionCount = 0;
 let questions = [];
 let currentQuestion = {};
-let correctAnswers = [];
+let correctAnswers = new Array();
+let userAnswers = new Array();
 
 var myHeaders = new Headers();
 myHeaders.append("Cookie", "XSRF-TOKEN=eyJpdiI6ImFxdFhIWG1Db2MySVUxTEU1MldNdnc9PSIsInZhbHVlIjoiK2Y4VUJhVVBSUGF6Zmcxc3BTRWp2UCtqVVJMTllxQW1yazl5SzlBaTFJUmhcL1BzNlwvY01cL1FBRTQ4M1h0ZmdSWTNnd01YczF4TU5kdjU0Y3hCTEVBd0lld3FwajhibkRoWVN0V1VpTmRROUVPSWpoM1czUTc3ODFhaU1PYUNCOG8iLCJtYWMiOiIyMmRhNmNlMzkxZTgzNTVmMDNlMDY2ZmIwNTEwMWFiZGYyMTNiZmZiMWQ5YThhNWFmZDNjMDJlMWQyZThmYmRiIn0%3D; quziapi_session=eyJpdiI6IkVuMG1UdWNRdEhXUTJzS1NvM0N2QXc9PSIsInZhbHVlIjoiVTFZVm1cL3dlNHNEbzF3RWZRUU96QitMUlFFQm1lTlhsMnJ5ZGRmd1ZodXBuZ2JaM015VWlBTnV6eGVBVk5CUUdOR3VZVkVSTE1BUm1QcVNvWWxCM0trTWNlbnp1M01NUTE1RUNydHZYc2daa0FnQTdMY1dJWDkwTXlrTHhneWpkIiwibWFjIjoiZTQyZjFjNzAwNWQyNGUyZmUxODUxYzMyNGI4ZjgyZDUxMzJmNjdlYjQwMjU2NWY1OTQ3Mzc3YzQ4NmQzZjhjNyJ9");
@@ -30,86 +30,93 @@ fetch("https://quizapi.io/api/v1/questions?limit=10&apiKey=q6NHZAHRcFHFl0runQZzu
       let questionDisplay = document.createElement('div');
      let question = document.createElement('h1');
      let quizContainer = document.getElementById('quiz');
-    const questionIndex = i;
-     currentQuestion = availableQuestions[questionIndex];
+     currentQuestion = availableQuestions[i];
      question.innerText = currentQuestion.question;
      questionDisplay.append(question);
      quizContainer.append(questionDisplay);
 
-    // console.log(result[0].correct_answers.answer_a_correct)
-
      let answerContainer = document.createElement('ul');
           answerContainer.setAttribute("style", "list-style-type: none");
 
-          let counter = 0;
-          // let correctAnswers = {}; 
-          
-          let radioID = `${currentQuestion.id.toString()}_${counter}`;
 
-      for (const [key, value] of Object.entries(currentQuestion.answers)) {
-        //  console.log(`${key} ${value}`);
-        if (value != null) {
-          let choiceLine = document.createElement("li");
-          choiceLine.textContent = " " + value;
-          choiceLine.setAttribute("for", key);
+            let correctValue = Object.values(currentQuestion.correct_answers);
+          let tempAnswers = Object.entries(currentQuestion.answers)
+            for (j=0; j<tempAnswers.length; j++){
+              if(tempAnswers[j][1] != null) {
+           let choiceLine = document.createElement("li");
+          choiceLine.textContent = " " + tempAnswers[j][1];
+          choiceLine.setAttribute("for", tempAnswers[j][0]);
           const radioBtn = document.createElement('input');
           radioBtn.setAttribute("type", "radio");
           radioBtn.setAttribute("class", "Answer-choices");
           radioBtn.setAttribute("name", currentQuestion.id);
-          radioBtn.setAttribute("id", `${currentQuestion.id.toString()}_${counter}`)
-          counter++;
+          radioBtn.setAttribute("value", correctValue[j]);
           choiceLine.prepend(radioBtn);
           answerContainer.append(choiceLine);
           questionDisplay.append(answerContainer);
-        }
-        // const radios = document.getElementsByClassName('Answer-choices');
-        // for (j = 0; j < radios.length; j++){
-        //   currentRadio = radios[j];
-        //   if (currentRadio.checked){
-        //     console.log(radioBtn.id)
-        //   }
-        // }
+              }
+}
 
-      }
-    }
+      // for (const [key, value] of Object.entries(currentQuestion.answers)) {
+
+      //   //  console.log(`${key} ${value}`);
+      //   // if (value != null) {
+      //     let choiceLine = document.createElement("li");
+      //     choiceLine.textContent = " " + value;
+      //     choiceLine.setAttribute("for", key);
+      //     const radioBtn = document.createElement('input');
+      //     radioBtn.setAttribute("type", "radio");
+      //     radioBtn.setAttribute("class", "Answer-choices");
+      //     radioBtn.setAttribute("name", currentQuestion.id);
+      //     console.log("i is ", i);
+      //     radioBtn.setAttribute("value", correctValue[i]);
+      //     // radioBtn.setAttribute("id", `${currentQuestion.id.toString()}_${counter}`)
+      //     // counter++;
+      //     choiceLine.prepend(radioBtn);
+      //     answerContainer.append(choiceLine);
+      //     questionDisplay.append(answerContainer);
+      //   // }
+      // }
+
+      // for (const [key, value] of Object.entries(currentQuestion.correct_answers)) { 
+      //   if (value === "true") {
+      //     correctAnswers.push({currentQuestion.id: key});
+      //   }
+      // }
+    }   
     let radios = document.querySelectorAll('.Answer-choices');
-    let userAnswers = new Array();
-    for (let j = 0; j < radios.length; j++) {
-      radios[j].addEventListener("change", function() {
-          let val = this.id; // this == the clicked radio,
-          userAnswers.push(val)
-       });
-      
-      }
-      console.log(userAnswers)
-  
-  //  for (correctAnswers = 0; ) {
-  //     console.log(hi);
-  //  }
-  //  console.log(correctAnswers)    
-         
+    for(radio in radios) {
+      radios[radio].onclick = function() {
+        if (this.value === "true") {
+         console.log('correct')
+        } 
+      };
+    };
+
+    // for (let j = 0; j < radios.length; j++) {
+    //   radios[j].addEventListener("change", function() {
+    //       let val = this.id; // this == the clicked radio,
+    //       userAnswers.push(val)
+    //       console.log(userAnswers)
+    //       localStorage.setItem('userAnswers', userAnswers);
+    //    });
+    //   }
    })
   .catch(error => console.log('error', error));
+ 
+
+  console.log(correctAnswers)
 
   let submit = document.getElementById('submitBtn');
- 
+  let score = 0;
 
   submit.addEventListener('click', e => {
-    
+
+    incrementScore = num => {
+      score += num;
+      scoreText.textContent = score;
+      localStorage.setItem('mostRecentScore', score);
+    }
     window.location.assign('/end.html');
   })
- 
- 
-//      for(radio in radios) {
-//        radios[radio].onclick = function() {
-//           //  userAnswers.push(this.id);
-//            console.log('you clicked');
-         
-//          localStorage.setItem('mostRecentScore', score);
-//        };
-       
-// }
-  incrementScore = num => {
-    score += num;
-    // scoreText.textContent = score;
-  }
+
